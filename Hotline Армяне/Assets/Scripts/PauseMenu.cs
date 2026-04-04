4cb41e29679a9a3cf787df476de6f8377a869a4d
Nikita48168 <gujukify@gmail.com>
2026-04-04 20:21:46 +0300

    NewMenu
    a scene and a folder in sprites for the menu were added, as well as a menu and working buttons were made, as well as a new script for the menu.

---
+++ Hotline Армяне/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public bool PauseGame;
+    public GameObject pauseGameMenu;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (PauseGame)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseGameMenu.SetActive(false);
+        Time.timeScale = 1f;
+        PauseGame= false;
+    }
+
+    public void Pause()
+    {
+        pauseGameMenu.SetActive(true);
+        Time.timeScale = 0;
+        PauseGame = true;
+    }
+
+    public void LosdMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+}
