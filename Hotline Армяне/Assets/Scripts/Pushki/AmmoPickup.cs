0af45ae8107b76ff64fdf3a0864913dfcae0d117
Nikita48168 <gujukify@gmail.com>
2026-04-19 14:01:33 +0300

    добавить систему спавна оружия
    - SpawnManager: спавн случайной пушки раз в 3 минуты, патронов раз в 30 секунд
    - WeaponPickup: подбор оружия, замена текущего, передача WeaponData игроку
    
    - Player: добавлен метод AddAmmo()
    - Разная скорострельность через поле fireRate в WeaponData
    - Созданы префабы: PistolPickup
    
    - Настраиваемая область спавна через инспектор

---
+++ Hotline Армяне/Assets/Scripts/Pushki/AmmoPickup.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class AmmoPickup : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] private int ammoAmount = 10; // сколько патронов даёт один pickup
+    [SerializeField] private AudioClip pickupSound;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Player player = other.GetComponent<Player>();
+            if (player != null)
+            {
+                // Добавляем патроны текущему оружию
+                player.AddAmmo(ammoAmount);
+
+                // Звук (опционально)
+                AudioSource source = other.GetComponent<AudioSource>();
+                if (source != null && pickupSound != null)
+                    source.PlayOneShot(pickupSound);
+
+                Destroy(gameObject);
+            }
+        }
+    }
+}
